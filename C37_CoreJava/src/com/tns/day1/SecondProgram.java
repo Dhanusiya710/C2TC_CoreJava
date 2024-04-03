e6f74caa84f07e152a0041a1509ca183682e6839
Dhanusiya710 <147370760+Dhanusiya710@users.noreply.github.com>
2024-04-03 12:58:15 +0530

    Update SecondProgram.java

--- C37_CoreJava/src/com/tns/day1/SecondProgram.java
+++ C37_CoreJava/src/com/tns/day1/SecondProgram.java
@@ -1,5 +1,8 @@
 package com.tns.day1;
 
 public class SecondProgram {
-
+     public static void main(string[] args){
+       system.out.println("Second Program");
+       system.out.println("Welcome u all");
+     }  
 }
